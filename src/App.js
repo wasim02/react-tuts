f86d57c47234c212d99abd3351a95da639d9e219
Wasim <wasimshaikh.7706@gmail.com>
2021-07-04 14:52:54 +0530

    learn useState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count1, setCount1] = useState(10);
+  const [count2, setCount2] = useState(20);
   return (
     <div className="App">
-      <h2>React Tutorial</h2>
+      <h2>React Hooks Tutorial</h2>
+      <div>Count 1: {count1}</div>
+      <div>Count 2: {count2}</div>
+      <button onClick={() => { setCount1(c1 => c1 + 1); setCount2(c2 => c2 + 1); }}>+</button>
     </div>
   );
 }
